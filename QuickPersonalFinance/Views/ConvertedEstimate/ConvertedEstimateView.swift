01d59848a4866112070fa89fe9d1a5c679a068c1
Armando Brito <4656134+arbridev@users.noreply.github.com>
2023-07-02 21:19:21 -0400

    chore: update converted estimate view

--- QuickPersonalFinance/Views/ConvertedEstimate/ConvertedEstimateView.swift
+++ QuickPersonalFinance/Views/ConvertedEstimate/ConvertedEstimateView.swift
@@ -16,93 +16,99 @@ struct ConvertedEstimateView: View {
     var recurrence: Recurrence
     
     var body: some View {
-        VStack {
-            // MARK: Top bar
-            ModalViewUpperBar(
-                title: "converted.estimate.title",
-                dismiss: dismiss
-            )
-            Spacer()
-
-            // MARK: Control
-            if !viewModel.isRefreshing {
-                Picker(
-                    "converted.estimate.currency",
-                    selection: $viewModel.pickerSelectedCurrency
-                ) {
-                    ForEach(viewModel.currencyIDs, id: \.self) { currencyID in
-                        Text(currencyID)
-                            .font(.App.input)
-                            .tag(currencyID)
-                    }
-                }
-                .padding(.bottom, 4)
-                .font(.App.input)
-                .alert(viewModel.errorMessage ?? "", isPresented: $viewModel.isErrorPresented) {
-
-                }
-            } else {
-                ProgressView()
+        ZStack {
+            VStack {
+                // MARK: Top bar
+                ModalViewUpperBar(
+                    title: "converted.estimate.title",
+                    dismiss: dismiss
+                )
+                Spacer()
             }
 
-            Divider()
-
-            // MARK: Estimate table
             VStack {
-                Divider()
-                    .overlay(Color.Palette.dividerPrimary)
-                    .padding(.horizontal)
-                
-                HStack {
-                    Text("estimate.table.income")
-                    Spacer()
-                    Text("\(viewModel.incomeTotal.asCurrency(withID: viewModel.selectedCurrency))")
-                        .underline(true, color: .Palette.incomeAccent)
+                Spacer()
+
+                // MARK: Control
+                if !viewModel.isRefreshing {
+                    Picker(
+                        "converted.estimate.currency",
+                        selection: $viewModel.pickerSelectedCurrency
+                    ) {
+                        ForEach(viewModel.currencyIDs, id: \.self) { currencyID in
+                            Text(currencyID)
+                                .font(.App.input)
+                                .tag(currencyID)
+                        }
+                    }
+                    .padding(.bottom, 4)
+                    .font(.App.input)
+                    .alert(viewModel.errorMessage ?? "", isPresented: $viewModel.isErrorPresented) {
+
+                    }
+                } else {
+                    ProgressView()
                 }
-                .padding(.top)
-                .padding(.horizontal)
-                .font(.app(size: 18))
-                
-                HStack {
-                    Text("estimate.table.expense")
-                    Spacer()
-                    Text("\(viewModel.expenseTotal.asCurrency(withID: viewModel.selectedCurrency))")
-                        .underline(true, color: .Palette.expenseAccent)
-                }
-                .padding(.top, 6)
-                .padding(.horizontal)
-                .font(.app(size: 18))
-                
+
                 Divider()
-                    .overlay(Color.Palette.dividerSecondary)
+
+                // MARK: Estimate table
+                VStack {
+                    Divider()
+                        .overlay(Color.Palette.dividerPrimary)
+                        .padding(.horizontal)
+
+                    HStack {
+                        Text("estimate.table.income")
+                        Spacer()
+                        Text("\(viewModel.incomeTotal.asCurrency(withID: viewModel.selectedCurrency))")
+                            .underline(true, color: .Palette.incomeAccent)
+                    }
                     .padding(.top)
                     .padding(.horizontal)
-                
-                HStack {
-                    Text("estimate.table.total")
-                    Spacer()
-                    Text("\(viewModel.balance.asCurrency(withID: viewModel.selectedCurrency))")
-                        .underline(true, color: .Palette.estimateAccent)
+                    .font(.app(size: 18))
+
+                    HStack {
+                        Text("estimate.table.expense")
+                        Spacer()
+                        Text("\(viewModel.expenseTotal.asCurrency(withID: viewModel.selectedCurrency))")
+                            .underline(true, color: .Palette.expenseAccent)
+                    }
+                    .padding(.top, 6)
+                    .padding(.horizontal)
+                    .font(.app(size: 18))
+
+                    Divider()
+                        .overlay(Color.Palette.dividerSecondary)
+                        .padding(.top)
+                        .padding(.horizontal)
+
+                    HStack {
+                        Text("estimate.table.total")
+                        Spacer()
+                        Text("\(viewModel.balance.asCurrency(withID: viewModel.selectedCurrency))")
+                            .underline(true, color: .Palette.estimateAccent)
+                    }
+                    .padding(.top)
+                    .padding(.horizontal)
+                    .font(.app(size: 20))
+
+                    HStack {
+                        Spacer()
+                        Text(viewModel.balanceInfoMessage)
+                            .font(.App.info)
+                    }
+                    .padding(.horizontal)
                 }
-                .padding(.top)
-                .padding(.horizontal)
-                .font(.app(size: 20))
-                
-                HStack {
-                    Spacer()
-                    Text(viewModel.balanceInfoMessage)
-                        .font(.App.info)
-                }
-                .padding(.horizontal)
+                Spacer()
             }
-            Spacer()
-        }
-        .onAppear {
-            viewModel.input(
-                financeData: mainData.financeData,
-                recurrence: recurrence,
-                moc: moc
-            )
+            .onAppear {
+                viewModel.input(
+                    financeData: mainData.financeData,
+                    recurrence: recurrence,
+                    moc: moc
+                )
+            }
         }
     }
 }
